a0233640f2956d51cf6cf8523e0112e202254603
theinhein09 <theinhein09@gmail.com>
2022-07-27 14:51:47 -0700

    style nav links

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,35 +1,83 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { Searchbar } from "./searchbar";
 import { IoMdHome, IoMdNotifications, IoMdMenu } from "react-icons/io";
 import { FaUsers } from "react-icons/fa";
 export function Navbar() {
   return (
     <header>
-      <nav className="shadow-md py-2 mb-2 text-sm">
-        <menu className="mx-auto max-w-fit">
+      <nav className="shadow-md py-2 mb-2 text-xs">
+        <menu className="mx-auto max-w-fit text-neutral-500">
           <li role="menuitem" className="inline-block px-3 py-1">
-            <Link to="/">
-              <IoMdHome className="text-2xl mx-auto" />
-              Home
-            </Link>
+            <NavLink to="/">
+              {({ isActive }) => (
+                <span
+                  className={
+                    isActive ? "text-blue-500 transition-all" : undefined
+                  }
+                >
+                  <IoMdHome
+                    className={`text-2xl mx-auto ${
+                      isActive ? "text-blue-500 transition-all" : undefined
+                    }`}
+                  />
+                  Home
+                </span>
+              )}
+            </NavLink>
           </li>
           <li role="menuitem" className="inline-block px-3 py-1">
-            <Link to="/friends">
-              <FaUsers className="text-2xl mx-auto" />
-              Friends
-            </Link>
+            <NavLink to="/friends">
+              {({ isActive }) => (
+                <span
+                  className={
+                    isActive ? "text-blue-500 transition-all" : undefined
+                  }
+                >
+                  <FaUsers
+                    className={`text-2xl mx-auto ${
+                      isActive ? "text-blue-500 transition-all" : undefined
+                    }`}
+                  />
+                  Friends
+                </span>
+              )}
+            </NavLink>
           </li>
           <li role="menuitem" className="inline-block px-3 py-1">
-            <Link to="/notifications">
-              <IoMdNotifications className="text-2xl mx-auto" />
-              Notifications
-            </Link>
+            <NavLink to="/notifications">
+              {({ isActive }) => (
+                <span
+                  className={
+                    isActive ? "text-blue-500 transition-all" : undefined
+                  }
+                >
+                  <IoMdNotifications
+                    className={`text-2xl mx-auto ${
+                      isActive ? "text-blue-500 transition-all" : undefined
+                    }`}
+                  />
+                  Notifications
+                </span>
+              )}
+            </NavLink>
           </li>
           <li role="menuitem" className="inline-block px-3 py-1">
-            <Link to="/menu">
-              <IoMdMenu className="text-2xl mx-auto" />
-              Menu
-            </Link>
+            <NavLink to="/menu">
+              {({ isActive }) => (
+                <span
+                  className={
+                    isActive ? "text-blue-500 transition-all" : undefined
+                  }
+                >
+                  <IoMdMenu
+                    className={`text-2xl mx-auto ${
+                      isActive ? "text-blue-500 transition-all" : undefined
+                    }`}
+                  />
+                  Menu
+                </span>
+              )}
+            </NavLink>
           </li>
         </menu>
       </nav>
