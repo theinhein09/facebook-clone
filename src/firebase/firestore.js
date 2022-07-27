5a1073c8635ea93a67400541b3955d1eab50cbcc
theinhein09 <theinhein09@gmail.com>
2022-07-26 22:38:43 -0700

    add switch for onsnapshot

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -14,12 +14,12 @@ import { app } from "./init";
 
 const db = getFirestore(app);
 
-const fillArray = (myArray) => {
-  const promises = myArray.map(async (item) => {
-    const user = await getDoc(doc(db, "users", item.data().publisher));
+const fetchPublisher = (feeds) => {
+  const promises = feeds.map(async (feed) => {
+    const user = await getDoc(doc(db, "users", feed.data().publisher));
     return {
-      id: item.id,
-      ...item.data(),
+      id: feed.id,
+      ...feed.data(),
       publisher: user.data(),
     };
   });
@@ -32,15 +32,20 @@ export class FS {
   }
 
   onSnapshot(set) {
-    const q = this.query("subscribers", "array-contains-any", [
-      "all",
-      auth._auth.currentUser.uid,
-    ]);
-    const unsubscribe = onSnapshot(q, async (querySnapshot) => {
-      const data = await fillArray(querySnapshot.docs);
-      set(data);
-    });
-    return unsubscribe;
+    switch (this.collection) {
+      case "feeds":
+        const q = this.query("subscribers", "array-contains-any", [
+          "all",
+          auth._auth.currentUser.uid,
+        ]);
+        const unsubscribe = onSnapshot(q, async (feeds) => {
+          const data = await fetchPublisher(feeds.docs);
+          set(data);
+        });
+        return unsubscribe;
+      default:
+        return new Error("ERROR");
+    }
   }
 
   query(prop = null, operator = null, value = null) {
