10e6a39679706d608a1203c93bd1d2dbbed59ffd
theinhein09 <theinhein09@gmail.com>
2022-07-29 05:57:36 -0700

    add account setting btn

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,86 +4,117 @@ import { FaUsers } from "react-icons/fa";
 import { Searchbar } from "./searchbar";
 import { useUserContextState } from "../contexts/user-context";
 import logo from "../assets/images/logo.png";
+import { RiSettings5Fill } from "react-icons/ri";
+import { FiLogOut } from "react-icons/fi";
+import { useBoolean } from "../hooks";
 export function Navbar() {
   const { user } = useUserContextState();
+  const [account, { toggle }] = useBoolean(false);
+
   return (
-    <nav className="shadow-md fixed w-full bg-white top-0 z-50 flex px-5">
-      <section className="flex items-center w-1/6 gap-2">
-        <div className="w-10 h-10 flex-none">
-          <img src={logo} alt="logo" width={40} height={40} />
-        </div>
-        <Searchbar />
-      </section>
-      <menu className="flex-grow text-neutral-500 flex justify-center">
-        <li role="menuitem" className="inline-block px-3 py-1">
-          <NavLink to="/">
-            {({ isActive }) => (
-              <span
-                className={
-                  isActive ? "text-blue-500 transition-all" : undefined
-                }
-              >
-                <IoMdHome
-                  className={`text-2xl mx-auto ${
+    <>
+      <nav className="shadow-md fixed w-full bg-white top-0 z-50 flex px-5">
+        <section className="flex items-center w-1/6 gap-2">
+          <div className="w-10 h-10 flex-none">
+            <img src={logo} alt="logo" width={40} height={40} />
+          </div>
+          <Searchbar />
+        </section>
+        <menu className="flex-grow text-neutral-500 flex justify-center">
+          <li role="menuitem" className="inline-block px-3 py-1">
+            <NavLink to="/">
+              {({ isActive }) => (
+                <span
+                  className={
                     isActive ? "text-blue-500 transition-all" : undefined
-                  }`}
-                />
-                Home
-              </span>
-            )}
-          </NavLink>
+                  }
+                >
+                  <IoMdHome
+                    className={`text-2xl mx-auto ${
+                      isActive ? "text-blue-500 transition-all" : undefined
+                    }`}
+                  />
+                  Home
+                </span>
+              )}
+            </NavLink>
+          </li>
+          <li role="menuitem" className="inline-block px-3 py-1">
+            <NavLink to="/friends">
+              {({ isActive }) => (
+                <span
+                  className={
+                    isActive ? "text-blue-500 transition-all" : undefined
+                  }
+                >
+                  <FaUsers
+                    className={`text-2xl mx-auto ${
+                      isActive ? "text-blue-500 transition-all" : undefined
+                    }`}
+                  />
+                  Friends
+                </span>
+              )}
+            </NavLink>
+          </li>
+          <li role="menuitem" className="inline-block px-3 py-1">
+            <NavLink to="/menu">
+              {({ isActive }) => (
+                <span
+                  className={
+                    isActive ? "text-blue-500 transition-all" : undefined
+                  }
+                >
+                  <IoMdMenu
+                    className={`text-2xl mx-auto ${
+                      isActive ? "text-blue-500 transition-all" : undefined
+                    }`}
+                  />
+                  Menu
+                </span>
+              )}
+            </NavLink>
+          </li>
+        </menu>
+        <section className="w-1/6 px-3 flex items-center justify-end">
+          <button className="bg-neutral-200 w-10 h-10 rounded-full mr-2">
+            <IoMdNotifications className="text-2xl mx-auto" />
+          </button>
+          <button onClick={toggle} className="w-10 h-10 " onC>
+            <img
+              src={user.profileUrl}
+              alt="avatar"
+              width={40}
+              height={40}
+              className="rounded-full object-center aspect-square"
+            />
+          </button>
+        </section>
+      </nav>
+      <menu
+        className={`right-3 mt-16 fixed p-2 bg-white ring-1 ring-neutral-200 rounded-md shadow-2xl ${
+          account ? "block" : "hidden"
+        }`}
+      >
+        <li className="text-left w-full py-2 pr-28 pl-2 text-sm text-neutral-700 rounded-md my-1 hover:bg-neutral-100 transition-all flex gap-4 items-center">
+          <div
+            role="presentation"
+            className="w-10 h-10 rounded-full bg-neutral-300 flex items-center justify-center text-2xl "
+          >
+            <RiSettings5Fill className="text-neutral-700" />
+          </div>
+          Settings
         </li>
-        <li role="menuitem" className="inline-block px-3 py-1">
-          <NavLink to="/friends">
-            {({ isActive }) => (
-              <span
-                className={
-                  isActive ? "text-blue-500 transition-all" : undefined
-                }
-              >
-                <FaUsers
-                  className={`text-2xl mx-auto ${
-                    isActive ? "text-blue-500 transition-all" : undefined
-                  }`}
-                />
-                Friends
-              </span>
-            )}
-          </NavLink>
-        </li>
-        <li role="menuitem" className="inline-block px-3 py-1">
-          <NavLink to="/menu">
-            {({ isActive }) => (
-              <span
-                className={
-                  isActive ? "text-blue-500 transition-all" : undefined
-                }
-              >
-                <IoMdMenu
-                  className={`text-2xl mx-auto ${
-                    isActive ? "text-blue-500 transition-all" : undefined
-                  }`}
-                />
-                Menu
-              </span>
-            )}
-          </NavLink>
+        <li className="text-left w-full py-2 pr-28 pl-2 text-sm text-neutral-700 rounded-md my-1 hover:bg-neutral-100 transition-all flex gap-4 items-center">
+          <div
+            role="presentation"
+            className="w-10 h-10 rounded-full bg-neutral-300 flex items-center justify-center text-2xl "
+          >
+            <FiLogOut className="text-neutral-700" />
+          </div>
+          Log Out
         </li>
       </menu>
-      <section className="w-1/6 px-3 flex items-center justify-end">
-        <button className="bg-neutral-200 w-10 h-10 rounded-full mr-2">
-          <IoMdNotifications className="text-2xl mx-auto" />
-        </button>
-        <button className="bg-neutral-200 w-10 h-10 rounded-full">
-          <img
-            src={user.profileUrl}
-            alt="avatar"
-            width={40}
-            height={40}
-            className="rounded-[100%] object-center aspect-square"
-          />
-        </button>
-      </section>
-    </nav>
+    </>
   );
 }
