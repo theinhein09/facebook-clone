70164c956d5296aa77aa5bf21b5cc4a1e694171e
theinhein09 <theinhein09@gmail.com>
2022-07-29 19:30:10 -0700

    remove cloudinary context

--- src/index.js
+++ src/index.js
@@ -4,16 +4,13 @@ import "./index.css";
 import App from "./app";
 import { UserContextProvider } from "./contexts/user-context";
 import { IconContext } from "react-icons";
-import { CloudinaryContext } from "cloudinary-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <UserContextProvider>
       <IconContext.Provider value={{ className: "text-neutral-400" }}>
-        <CloudinaryContext cloudName="dmkcfie45">
-          <App />
-        </CloudinaryContext>
+        <App />
       </IconContext.Provider>
     </UserContextProvider>
   </React.StrictMode>
