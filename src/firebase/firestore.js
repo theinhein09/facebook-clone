0438347da42f880d9d860f405585614d798c668f
theinhein09 <theinhein09@gmail.com>
2022-07-31 00:47:08 -0700

    remove FS

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -21,100 +21,6 @@ import { app } from "./init";
 
 const db = getFirestore(app);
 
-const fetchPublisher = (feeds) => {
-  const promises = feeds.map(async (feed) => {
-    const user = await getDoc(doc(db, "users", feed.data().publisher));
-    return {
-      id: feed.id,
-      ...feed.data(),
-      publisher: { id: user.id, ...user.data() },
-    };
-  });
-  return Promise.all(promises);
-};
-
-function fetchUsers(ids) {
-  const promises = ids.map(async (id) => {
-    const user = await getDoc(doc(db, "users", id));
-    return {
-      id: user.id,
-      ...user.data(),
-    };
-  });
-  return Promise.all(promises);
-}
-
-export class FS {
-  constructor(collection) {
-    this.collection = collection;
-    this.lastVisible = null;
-  }
-
-  async getDoc(id) {
-    const ref = doc(db, this.collection, id);
-    const docSnap = await getDoc(ref);
-    if (docSnap.exists()) {
-      return { id: docSnap.id, ...docSnap.data() };
-    }
-    return null;
-  }
-
-  async getDocs(param) {
-    const q = query(
-      collection(db, this.collection),
-      where("username-sl", "==", param)
-    );
-    const querySnapshot = await getDocs(q);
-    return querySnapshot.docs.map((doc) => ({
-      id: doc.id,
-      ...doc.data(),
-    }));
-  }
-
-  async addDoc(data) {
-    const ref = collection(db, this.collection);
-    return await addDoc(ref, { ...data, timestamp: serverTimestamp() });
-  }
-
-  async setDoc(id, data) {
-    const ref = doc(db, this.collection, id);
-    return await setDoc(ref, { ...data, timestamp: serverTimestamp() });
-  }
-
-  async deleteDoc(id) {
-    const ref = doc(db, this.collection, id);
-    await deleteDoc(ref);
-  }
-
-  async updateDoc(id, data) {
-    const ref = doc(db, this.collection, id);
-    await updateDoc(ref, {
-      ...data,
-      timestamp: serverTimestamp(),
-      edited: true,
-    });
-  }
-
-  async getNextDocs() {
-    const ref = collection(db, this.collection);
-    const q = query(
-      ref,
-      where("subscribers", "array-contains-any", [
-        "all",
-        auth._auth.currentUser.uid,
-      ]),
-      orderBy("timestamp", "desc"),
-      startAfter(this.lastVisible),
-      limit(2)
-    );
-
-    const nextDocs = await getDocs(q);
-    const lastVisible = nextDocs.docs[nextDocs.docs.length - 1];
-    this.lastVisible = lastVisible;
-    return await fetchPublisher(nextDocs.docs);
-  }
-}
-
 export class Users {
   static async getUserById(id) {
     const ref = doc(db, "users", id);
@@ -177,6 +83,33 @@ export class Users {
 
 export class Feeds {
   static lastVisible = null;
+
+  static async getFeedById(id) {
+    const ref = doc(db, "feeds", id);
+    const feed = await getDoc(ref);
+    if (feed.exists()) {
+      return {
+        id: feed.id,
+        ...feed.data(),
+      };
+    }
+    return null;
+  }
+
+  static async getFeedsByUserId(userId) {
+    const ref = collection(db, "feeds");
+    const q = query(
+      ref,
+      where("publisher", "==", userId),
+      orderBy("timestamp", "desc"),
+      limit(2)
+    );
+    const feeds = await getDocs(q);
+    return feeds.docs.map((feed) => ({
+      id: feed.id,
+      ...feed.data(),
+    }));
+  }
 
   static async addFeed(feed) {
     const ref = collection(db, "feeds");
@@ -247,7 +180,6 @@ export class Feeds {
         publisher: Users.getUserById(feed.data().publisher),
       });
     }
-
     return feeds;
   }
 }
