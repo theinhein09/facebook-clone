052633741b2cb610db2d8683e5a4e5afb2f119bb
theinhein09 <theinhein09@gmail.com>
2022-07-31 00:57:35 -0700

    remove firebase config file

--- src/firebase/config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBCYxljhUWk9IYMoQ__P_XnFlyB9OnIzFw",
-  authDomain: "social-media-6c6d6.firebaseapp.com",
-  databaseURL: "https://social-media-6c6d6-default-rtdb.firebaseio.com",
-  projectId: "social-media-6c6d6",
-  storageBucket: "social-media-6c6d6.appspot.com",
-  messagingSenderId: "529710992573",
-  appId: "1:529710992573:web:59b5c2c09f4f16a4fabbd4",
-};
