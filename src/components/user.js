54d4a60a96e2ca87ceeff085881ee62339a646fd
theinhein09 <theinhein09@gmail.com>
2022-07-31 03:25:53 -0700

    add link btn to user component

--- src/components/user.js
+++ src/components/user.js
@@ -1,21 +1,24 @@
+import { Link } from "react-router-dom";
 import { Image } from "./image";
 
 export function User({ user, status }) {
   return (
-    <section className="py-2 flex items-center">
-      <div
-        role="presentation"
-        className="max-w-10 max-h-40 ml-1 mr-2 rounded-full"
-      >
-        <Image
-          publicId={user.profileUrl}
-          transform={{ type: "profile-pic", width: 40, height: 40 }}
-        />
-      </div>
-      <div className="text-sm font-light flex-col gap-2">
-        <h3 className="font-semibold">{user.username}</h3>
-        <div className="text-xs">{status}</div>
-      </div>
-    </section>
+    <Link to={`/${user.id}/posts`}>
+      <section className="py-2 flex items-center">
+        <div
+          role="presentation"
+          className="max-w-10 max-h-40 ml-1 mr-2 rounded-full"
+        >
+          <Image
+            publicId={user.profileUrl}
+            transform={{ type: "profile-pic", width: 40, height: 40 }}
+          />
+        </div>
+        <div className="text-sm font-light flex-col gap-2">
+          <h3 className="font-semibold">{user.username}</h3>
+          <div className="text-xs">{status}</div>
+        </div>
+      </section>
+    </Link>
   );
 }
