4efb3f2269cb38523a104ce57e346b664822cb52
theinhein09 <theinhein09@gmail.com>
2022-08-01 07:12:46 -0700

    refactor feed options menu

--- src/components/feed-options-menu.js
+++ src/components/feed-options-menu.js
@@ -4,6 +4,7 @@ import { Feeds } from "../firebase/firestore";
 import { useBoolean } from "../hooks";
 import { Dialog } from "./dialog";
 import { Modal } from "./modal";
+
 export function FeedOptionsMenu({ feed }) {
   const [optionsMenu, { toggle: toggleOptionsMenu }] = useBoolean(false);
   const [editingFeed, setEditingFeed] = useState({ text: "" });
@@ -18,15 +19,6 @@ export function FeedOptionsMenu({ feed }) {
   async function handleDelete() {
     await Feeds.deleteFeed(feed.id);
     toggleOptionsMenu();
-  }
-
-  function handleEditingFeedChange({ target }) {
-    setEditingFeed({ ...editingFeed, text: target.value });
-  }
-
-  async function handleEditingFeed() {
-    await Feeds.updateFeed(feed.id, editingFeed);
-    toggleFeedEditor();
   }
 
   return (
@@ -59,38 +51,58 @@ export function FeedOptionsMenu({ feed }) {
         </menu>
       </div>
       {feedEditor ? (
-        <Modal toggle={toggleFeedEditor}>
-          <Dialog>
-            <div role="presentation" className="min-w-[320px] rounded bg-white">
-              <h3 className="mb-2 rounded-t bg-blue-500 px-2 py-1 text-white">
-                Edit Post
-              </h3>
-              <section className="w-full text-center">
-                <textarea
-                  value={editingFeed.text}
-                  onChange={handleEditingFeedChange}
-                  className="my-2 w-[300px] resize-none p-1 text-neutral-500"
-                  rows={5}
-                />
-              </section>
-              <section className="py-2 px-1 text-right">
-                <button
-                  onClick={handleEditingFeed}
-                  className="mr-1 rounded-full bg-blue-400 px-4 py-1 text-sm text-white shadow-md transition-all hover:bg-blue-500 hover:shadow-lg"
-                >
-                  Update
-                </button>
-                <button
-                  onClick={toggleFeedEditor}
-                  className="rounded-full bg-neutral-400 px-4 py-1 text-sm text-white shadow-md transition-all hover:bg-neutral-500 hover:shadow-lg"
-                >
-                  Cancel
-                </button>
-              </section>
-            </div>
-          </Dialog>
-        </Modal>
+        <FeedEditor
+          feed={feed}
+          toggle={toggleFeedEditor}
+          editingFeed={editingFeed}
+          setEditingFeed={setEditingFeed}
+        />
       ) : null}
     </>
+  );
+}
+
+function FeedEditor({ toggle, feed, editingFeed, setEditingFeed }) {
+  function handleEditingFeedChange({ target }) {
+    setEditingFeed({ ...editingFeed, text: target.value });
+  }
+
+  async function handleEditingFeed() {
+    await Feeds.updateFeed(feed.id, editingFeed);
+    toggle();
+  }
+
+  return (
+    <Modal toggle={toggle}>
+      <Dialog>
+        <div role="presentation" className="min-w-[320px] rounded bg-white">
+          <h3 className="mb-2 rounded-t bg-blue-500 px-2 py-1 text-white">
+            Edit Post
+          </h3>
+          <section className="w-full text-center">
+            <textarea
+              value={editingFeed.text}
+              onChange={handleEditingFeedChange}
+              className="my-2 w-[300px] resize-none p-1 text-neutral-500"
+              rows={5}
+            />
+          </section>
+          <section className="py-2 px-1 text-right">
+            <button
+              onClick={handleEditingFeed}
+              className="mr-1 rounded-full bg-blue-400 px-4 py-1 text-sm text-white shadow-md transition-all hover:bg-blue-500 hover:shadow-lg"
+            >
+              Update
+            </button>
+            <button
+              onClick={toggle}
+              className="rounded-full bg-neutral-400 px-4 py-1 text-sm text-white shadow-md transition-all hover:bg-neutral-500 hover:shadow-lg"
+            >
+              Cancel
+            </button>
+          </section>
+        </div>
+      </Dialog>
+    </Modal>
   );
 }
