30ab17003aa18b86b03ea565903c42e0a364c585
theinhein09 <theinhein09@gmail.com>
2022-07-31 18:45:43 -0700

    refactor upload widget

--- src/components/upload.js
+++ src/components/upload.js
@@ -1,62 +1,74 @@
 import { useEffect, useRef } from "react";
 
+function createWidget(folder) {
+  const media = [];
+  const defaultOptions = {
+    folder,
+    cloudName: "dmkcfie45",
+    uploadPreset: "sssaoyjo",
+    multiple: true,
+    maxFiles: 5,
+    defaultSource: "local",
+    clientAllowedFormats: ["webp", "gif", "jpg", "png", "jpeg"],
+    resourceType: "image",
+    showAdvancedOptions: false,
+    styles: {
+      palette: {
+        window: "#ffffff",
+        sourceBg: "#f4f4f5",
+        windowBorder: "#90a0b3",
+        tabIcon: "#000000",
+        inactiveTabIcon: "#555a5f",
+        menuIcons: "#555a5f",
+        link: "#0094EC",
+        action: "#339933",
+        inProgress: "#0433ff",
+        complete: "#339933",
+        error: "#cc0000",
+        textDark: "#000000",
+        textLight: "#fcfffd",
+      },
+      fonts: {
+        default: null,
+        "'Poppins', sans-serif": {
+          url: "https://fonts.googleapis.com/css?family=Poppins",
+          active: true,
+        },
+      },
+    },
+  };
+  return {
+    widget: window.cloudinary.createUploadWidget(
+      defaultOptions,
+      (error, result) => {
+        if (!error && result && result.event === "success") {
+          console.log("Done! Here is the image info: ", result.info);
+          media.push(result.info.public_id);
+        }
+      }
+    ),
+    media,
+  };
+}
+
 export default function CloudinaryUploadWidget({ setPost, folder }) {
   const widget = useRef(null);
-  const media = useRef([]);
 
   useEffect(() => {
-    return () => widget.current && widget.current.destroy();
+    if (widget.current === null) return;
+    const currentWidget = widget.current;
+    return () => currentWidget.destroy();
   }, []);
 
   function handleUpload(evt) {
     evt.preventDefault();
-    widget.current = window.cloudinary.createUploadWidget(
-      {
-        cloudName: "dmkcfie45",
-        uploadPreset: "sssaoyjo",
-        multiple: true,
-        maxFiles: 5,
-        folder,
-        defaultSource: "local",
-        clientAllowedFormats: ["webp", "gif", "jpg", "png", "jpeg"],
-        resourceType: "image",
-        showAdvancedOptions: false,
-        styles: {
-          palette: {
-            window: "#ffffff",
-            sourceBg: "#f4f4f5",
-            windowBorder: "#90a0b3",
-            tabIcon: "#000000",
-            inactiveTabIcon: "#555a5f",
-            menuIcons: "#555a5f",
-            link: "#0094EC",
-            action: "#339933",
-            inProgress: "#0433ff",
-            complete: "#339933",
-            error: "#cc0000",
-            textDark: "#000000",
-            textLight: "#fcfffd",
-          },
-          fonts: {
-            default: null,
-            "'Poppins', sans-serif": {
-              url: "https://fonts.googleapis.com/css?family=Poppins",
-              active: true,
-            },
-          },
-        },
-      },
-      (error, result) => {
-        if (!error && result && result.event === "success") {
-          console.log("Done! Here is the image info: ", result.info);
-          media.current.push(result.info.public_id);
-          setPost((post) => ({
-            ...post,
-            media: media.current,
-          }));
-        }
-      }
-    );
+    const { widget, media } = createWidget(folder);
+
+    widget.current = widget;
+    setPost((post) => ({
+      ...post,
+      media,
+    }));
     widget.current.open();
   }
 
